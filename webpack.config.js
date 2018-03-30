a6150257e38c23ef32ce5d9d78cbdb77ee73360c
Liu Shuo <liu@shuo.email>
2018-03-30 11:17:09 +0800

    webpack-dev-server use https

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const fs = require('fs')
 const config = {
   entry: [
   './index.js'
@@ -11,6 +12,11 @@ const config = {
     contentBase: path.join(__dirname, './dist'),
     host: '0.0.0.0',
     port: 8000,
+    https: {
+      key: fs.readFileSync('./cert/server.key'),
+      cert: fs.readFileSync('./cert/server.crt'),
+      ca: fs.readFileSync('./cert/ca.crt')
+    }
   },
   watch: true,
   mode: 'development'
