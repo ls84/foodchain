c400557173a45431d407d6b01fd89de97246f944
Liu Shuo <liu@shuo.email>
2018-07-07 15:46:50 +0800

    append a signButton

--- src/consumptionView.js
+++ src/consumptionView.js
@@ -2,9 +2,35 @@ const JSS = jss.create()
 JSS.setup({createGenerateClassName: () => (rule, sheet) => rule.key})
 JSS.use(jssNested.default())
 
-const styles = {}
+const styles = {
+  'signButton': {
+    'height': '30px',
+    'width': '60px',
+    'position': 'relative',
+    'left': 'calc(100% - 60px)',
+    'line-height': '30px',
+    'text-align': 'center',
+    'background-color': 'white',
+    'margin-top': '10px',
+    'margin-bottom': '20px',
+    'font-family': 'sans-serif',
+    'color': 'lightgrey',
+    '&.active': {
+      'color': 'black'
+    }
+  }
+}
 
 const styleSheet = JSS.createStyleSheet(styles)
+
+function SignButton() {
+  let sign = document.createElement('div')
+  sign.className = 'signButton'
+  
+  sign.textContent = 'Sign'
+
+  return sign
+}
 
 export default class consumptionView extends HTMLElement {
   constructor () {
@@ -16,5 +42,8 @@ export default class consumptionView extends HTMLElement {
 
     this.consumptionEditor = document.createElement('consumption-editor')
     this.shadow.appendChild(this.consumptionEditor)
+
+    this.signButton = SignButton()
+    this.shadow.appendChild(this.signButton)
   }
 }
